6ab61510aafdad0a5e5086ea817c34a1b04d5310
TranThoiLong <92561556+SnikGaming@users.noreply.github.com>
2023-03-09 01:26:14 +0700

    Create http_LayDataTheoId.dart

---
+++ http_LayDataTheoId.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  final jsonData = jsonDecode(response.body);
+  print(jsonData);
+}
+Phương thức `http.get()` được sử dụng để gửi yêu cầu GET đến URL được cung cấp.
+Phương thức `jsonDecode()` được sử dụng để chuyển đổi dữ liệu JSON (chứa trong phản hồi) thành một đối tượng Dart map.
+`await` được sử dụng để đợi phản hồi trả về trước khi tiếp tục thực hiện các câu lệnh khác trong hàm `main()`.
+Dữ liệu JSON được in ra màn hình sử dụng phương thức `print()`.
